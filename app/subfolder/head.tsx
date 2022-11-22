bb864792042cd3de673ab2e7589a28ef18c5be13
Daniel Wei <dlw266@cornell.edu>
2022-11-21 21:41:41 -0500

    Working Header

--- app/subfolder/head.tsx
+++ app/subfolder/head.tsx
@@ -1,14 +1,7 @@
-async function getPost(slug: string) {
-    const res = await fetch("...");
-    return res.json();
-}
-
 export default async function Head({ params }: { params: { slug: string } }) {
-    const post = await getPost(params.slug);
-
     return (
         <>
-            <title>{post.title}</title>
+            <title>Subpage</title>
         </>
     );
 }
